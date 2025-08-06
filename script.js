b71008ad59c4e75325f05fef6beee7337314556e
kuutoke <rubysinger28@gmail.com>
2025-08-06 13:57:14 -0700

    JS Test 0.2
    finished applying functions, still inoperable

--- script.js
+++ script.js
@@ -6,16 +6,6 @@ const token = `pathBADIOjtLtrliZ.79eb3cd8ca485b826778840b03b8a714c7e1b8dc18180bb
 const baseId = `app9TUcYzYbLtSc81`;
 const tableId = `tblQjuvCaEFjawQYE`;
 
-//Eventually will make functions to sort, but let's start here
-//template first? i dont know what im doing
-
-class Grid {
-  constructor(title, thumb, type) {
-    this.title = title;
-    this.thumb = thumb;
-    this.type = type;
-  }
-}
 
 let gridItems = [];
 
@@ -65,12 +55,14 @@ const filterData = (data, subject, category) => {
 const renderDrawings = (rows, category) => {
 	let innerHTML = "";
 	rows.forEach((row) => {
-		const article = `
-		  <article>
-		    <h2>${row.fields["name"]}</h2>
-			<p>${row.fields["additional_info"]}</p>
-		  </article>`;
-		innerHTML += article;
+		const card = `
+		  <div class="griditem col-sm">
+				<h2>${row.fields["fldc10uWJMs8Kbwfp"]}</h2>
+				<img src="${row.fields["fldls2x0DbFSSZ3A5"]}" alt="Image unavailable.">
+				<p>${row.fields["fldoISxwtISg14mGW"]}</p>
+			</div>
+		`;
+		innerHTML += card;
 	});
 	return innerHTML;
 };
@@ -78,9 +70,10 @@ const renderGeneral = (rows, category) => {
 	let innerHTML = "";
 	rows.forEach((row) => {
 		const article = `
-		  <article>
-		    <h2>${row.fields["name"]}</h2>
-			<p>${row.fields["additional_info"]}</p>
+		  <article class="griditem">
+		    <h2>${row.fields["fldc10uWJMs8Kbwfp"]}</h2>
+			<img src="${row.fields["fldls2x0DbFSSZ3A5"]}" alt="Image unavailable.">
+			<p>${row.fields["fldoISxwtISg14mGW"]}</p>
 		  </article>`;
 		innerHTML += article;
 	});
@@ -90,81 +83,39 @@ const renderWorldbuilding = (rows, category) => {
 	let innerHTML = "";
 	rows.forEach((row) => {
 		const article = `
-		  <article>
-		    <h2>${row.fields["name"]}</h2>
-			<p>${row.fields["additional_info"]}</p>
+		  <article class="griditem">
+		    <h2>${row.fields["fldc10uWJMs8Kbwfp"]}</h2>
+			<img src="${row.fields["fldls2x0DbFSSZ3A5"]}" alt="Image unavailable.">
+			<p>${row.fields["fldoISxwtISg14mGW"]}</p>
 		  </article>`;
 		innerHTML += article;
 	});
 	return innerHTML;
 };
 
-/*		const data = await response.json();
-		
-		let content = "",
-		for (const record of data.records) {
-			const title = record.fields.name || "Title not available";
-			const thumb = record.fields.fldls2x0DbFSSZ3A5 || "Image not available";
-			const type = record.fields.type || "Type not available";
-			
-			//get image url
-			const thumbUrl = thumb ? fldls2x0DbFSSZ3A5[0]?.url;
-			
-			content += `
-				<div>
-					<h3>${name}</h3>
-					<img src="${thumbUrl}" />
-					<p>${type}</p>
-				</div>
-			`;
+//main function
+document.addEventListener("DOMContentLoaded", async () => {
+	const queryParams = getAllQueryParams();
+	console.log(`queryParams: ${queryParams}`);
+	
+	if (queryParams.subject && queryParams.category) {
+		const subject = queryParams.subject;
+		const category = queryParams.category;
+		const data = await fetchTable(baseId, tableId, token);
+		const filteredData = filterData(data, subject, category);
+		const element = document.getElementById('gridbody');
+		let innerHTML = "";
+		switch (subject) {
+			case "drawing":
+				innerHTML = renderDrawings(filteredData, category);
+				break;
+			case "general":
+				innerHTML = renderGeneral(filteredData, category);
+				break;
+			case "worldbuilding"
+				innerHTML = renderWorldbuilding(filteredData, category);
+				break;
 		}
-		
-		container.innerHTML = content;
-	} catch (error) {
-		console.log("Error:", error);
-		container.innerHTML = "<p>Failed to fetch data. Please try again later.</p>";
+		element.innerHTML = innerHTML;
 	}
-};
-
-getAllData();
-	*/
-
-/* Old code that I have no idea if it works. didn't for me but its incomplete
-
-async function getAllRecords() {
-  let getResultElement = document.getElementById("drawing");
-
-  const options = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer pathBADIOjtLtrliZ.79eb3cd8ca485b826778840b03b8a714c7e1b8dc18180bb5f95afb38aebf371d`,
-    },
-  };
-
-  await fetch(
-    `//https://api.airtable.com/v0/app9TUcYzYbLtSc81/Reference%20Links`,
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-
-      getResultElement.innerHTML = "";
-
-      let newHTML = "",
-
-      for (let i=0; i < data.records.length; i++) {
-        let name = data.records[i].fields["fldc10uWJMs8Kbwfp"];
-        let thumb = data.records[i].fields["fldls2x0DbFSSZ3A5"];
-        let category = data.records[i].fields["fldPP4tFHv24zKqxp"];
-
-        newHtml += `
-          <div class="thumb">
-            <p><a href="drawing.html"
-        `
-
-      }
-    })
-}
-
-*/
+});
